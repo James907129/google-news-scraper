8065cb138d748d60940d6e5f6fa846cf8f94649c
James907129 <laurenmorgan9865@iejesusmirey.com>
2025-11-11 19:58:21 +0500

    feat: add scraper.py

---
+++ scraper.py
@@ -0,0 +1,5 @@
+3. The data will be saved in the `output.json` file in the `data/` folder.
+
+## Configuration
+
+Edit the `settings.example.json` file to set the query parameters such as start date, end date, and search query.
